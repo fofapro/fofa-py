c1bdceb5e5ef2d3b1bf043de13addf48741a777d
Loveforkeeps <loveforkeeps@foxmail.com>
2020-09-07 14:49:51 +0800

    Update client.py

--- fofa/client.py
+++ fofa/client.py
@@ -4,13 +4,15 @@
 import base64
 import json
 import requests
-import os,sys
+import os
+import sys
 
 # Python版本识别
 if sys.version > '3':
     PY3 = True
 else:
     PY3 = False
+
 
 class Client:
     def __init__(self, email, key, debug=False):
@@ -26,20 +28,20 @@ class Client:
         api_full_url = "%s%s" % (self.base_url, self.login_api_url)
         param = {"email": self.email, "key": self.key}
         if self.debug:
-            print(api_full_url,param)
+            print(api_full_url, param)
         res = self.__http_get(api_full_url, param)
         return json.loads(res)
 
-    def get_data(self, query_str, page=1, fields=""):
+    def get_data(self, query_str, page=1, fields="", size=100):
         res = self.get_json_data(query_str, page, fields)
         return json.loads(res)
 
-    def get_json_data(self, query_str, page=1, fields=""):
+    def get_json_data(self, query_str, page=1, fields="", size=100):
         api_full_url = "%s%s" % (self.base_url, self.search_api_url)
         if PY3:
             query_str = query_str.encode()
-        param = {"qbase64": base64.b64encode(
-            query_str), "email": self.email, "key": self.key, "page": page, "fields": fields}
+        query_str = base64.b64encode(query_str)
+        param = {"qbase64": query_str, "email": self.email, "key": self.key, "page": page, "fields": fields, "size": size}
         res = self.__http_get(api_full_url, param)
         return res
 
@@ -59,5 +61,5 @@ if __name__ == "__main__":
     env_dist = os.environ
     FOFA_EMAIL = env_dist.get('FOFA_EMAIL')
     FOFA_KEY = env_dist.get('FOFA_KEY')
-    client = Client(FOFA_EMAIL, FOFA_KEY,debug=True)
-    client.get_json_data('app="网宿"',page=1)
+    client = Client(FOFA_EMAIL, FOFA_KEY, debug=True)
+    client.get_data('app="网宿"', page=1)
