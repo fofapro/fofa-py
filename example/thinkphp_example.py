5afaad82549e3f9aaa2d44296274375a118a32b3
yujia liu <liuyujia1@360.cn>
2022-05-06 16:39:26 +0800

    fix

--- example/thinkphp_example.py
+++ example/thinkphp_example.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     query_str = 'header="thinkphp" || header="think_template"'
     for page in range(1,51):
         # fcoin = client.get_userinfo()["fcoin"]
-        # if fcoin >= 249:
+        # if fcoin <= 249:
         #     break
         data = client.get_data(query_str,size=100,page=page,fields="ip,city")
         for ip,city in data["results"]:
